dee186044f5626cbe2c3c942c8a87cba59da420c
Deep Majumder <deep.majumder2019@gmail.com>
2021-05-28 00:37:15 +0530

    Basic idea behind a resource

---
+++ src/resource.rs
@@ -0,0 +1,28 @@
+use crate::uses::*;
+
+/// The fully-qualified path to a resource.
+/// A resource may be a struct or a function.
+pub type ResourceID = UsePath;
+
+/// SingleResource refers to a single struct, which
+/// we want to consider as a resource.
+///
+/// The `creators` field contains identifiers of functions
+/// (aka methods) which create (or construct) such a resource.
+pub struct SingleResource {
+    id: ResourceID,
+    creators: Vec<Creator>,
+}
+
+/// Creator refers to functions which create a resource.
+///
+/// A `Direct` creator is one like `Arc::new()` or `Notify::new()`.
+/// Such a function directly returns a resource.
+///
+/// A `Tuple` creator is one like `oneshot::channel()`. This returns
+/// both the sender and the reciever as a tuple. Hence, the second arg
+/// is the index of the tuple we are interested in.
+pub enum Creator {
+    Direct(ResourceID),
+    Tuple(ResourceID, usize),
+}
