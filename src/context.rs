4d9bb1f930e460fdfd32421f15093fd7125f7f97
Deep Majumder <deep.majumder2019@gmail.com>
2021-06-03 18:29:36 +0530

    Added iterator over use paths to Context

--- src/context.rs
+++ src/context.rs
@@ -22,12 +22,12 @@ impl Context {
 
     pub fn enter_block(&mut self) {
         self.use_paths.push(Vec::new());
-        // self.env.enter_block();
+        self.env.enter_block();
     }
 
     pub fn exit_block(&mut self) {
         self.use_paths.pop();
-        // self.env.exit_block();
+        self.env.exit_block();
     }
 
     /// Adds paths to the current block.
@@ -37,16 +37,161 @@ impl Context {
         let mut paths = paths;
         last.append(&mut paths);
     }
+
+    pub fn iter(&self) -> PathIter<'_> {
+        PathIter::new(&self)
+    }
 }
 
-pub struct Environment {
-    id_map: HashMap<String, Vec<Object>>,
+pub struct PathIter<'ctx> {
+    block_idx: usize,
+    path_idx: usize,
+    ctx: &'ctx Context,
+}
+
+impl<'ctx> PathIter<'ctx> {
+    fn new(ctx: &'ctx Context) -> Self {
+        Self {
+            block_idx: 0,
+            path_idx: 0,
+            ctx,
+        }
+    }
+}
+
+impl PathIter<'_> {
+    fn advance(&mut self) -> bool {
+        let block = self.ctx.use_paths.get(self.block_idx);
+        if let Some(block) = block {
+            self.path_idx += 1;
+            let mut block = Some(block);
+            while block.is_some() && self.path_idx == block.unwrap().len() {
+                self.block_idx += 1;
+                self.path_idx = 0;
+                block = self.ctx.use_paths.get(self.block_idx);
+            }
+            block.is_some()
+        } else {
+            false
+        }
+    }
+}
+
+impl<'ctx> Iterator for PathIter<'ctx> {
+    type Item = &'ctx UsePath;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let item = {
+            let block = self.ctx.use_paths.get(self.block_idx);
+            if let Some(block) = block {
+                block.get(self.path_idx)
+            } else {
+                None
+            }
+        };
+        self.advance();
+        item
+    }
+}
+
+struct Environment {
+    block_envs: Vec<BlockEnvironment>,
+}
+
+struct BlockEnvironment {
+    id_map: HashMap<String, Object>,
 }
 
 impl Environment {
     fn new() -> Self {
+        let mut block_envs = Vec::new();
+        block_envs.push(BlockEnvironment::new()); // For the global scope
+        Self { block_envs }
+    }
+
+    fn enter_block(&mut self) {
+        self.block_envs.push(BlockEnvironment::new());
+    }
+
+    fn exit_block(&mut self) {
+        self.block_envs.pop();
+    }
+}
+
+impl BlockEnvironment {
+    fn new() -> Self {
         Self {
             id_map: HashMap::new(),
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::path;
+
+    #[test]
+    fn test_empty_ctx() {
+        let ctx = Context::new();
+        assert_eq!(ctx.iter().next(), None);
+    }
+
+    #[test]
+    fn test_ctx_with_one_block() {
+        let mut ctx = Context::new();
+        ctx.add_use_paths(vec![
+            path!["std", "fs", "File"],
+            path!["std", "path", "Path"],
+        ]);
+        let paths: Vec<&UsePath> = ctx.iter().collect();
+        assert_eq!(paths.len(), 2);
+        assert_eq!(paths[0], &path!["std", "fs", "File"]);
+        assert_eq!(paths[1], &path!["std", "path", "Path"]);
+    }
+
+    #[test]
+    fn test_ctx_with_two_blocks() {
+        let mut ctx = Context::new();
+        ctx.add_use_paths(vec![
+            path!["std", "fs", "File"],
+            path!["std", "path", "Path"],
+        ]);
+        ctx.enter_block();
+        ctx.add_use_paths(vec![
+            path!["std", "path", "PathBuf"],
+            path!["std", "collections", "HashMap"],
+        ]);
+        let paths: Vec<&UsePath> = ctx.iter().collect();
+        assert_eq!(paths.len(), 4);
+        assert_eq!(paths[0], &path!["std", "fs", "File"]);
+        assert_eq!(paths[1], &path!["std", "path", "Path"]);
+        assert_eq!(paths[2], &path!["std", "path", "PathBuf"]);
+        assert_eq!(paths[3], &path!["std", "collections", "HashMap"]);
+    }
+
+    #[test]
+    fn test_ctx_with_empty_blocks() {
+        let mut ctx = Context::new();
+        ctx.add_use_paths(vec![
+            path!["std", "fs", "File"],
+            path!["std", "path", "Path"],
+        ]);
+        ctx.enter_block();
+        ctx.enter_block();
+        ctx.enter_block();
+        ctx.enter_block();
+        ctx.add_use_paths(vec![
+            path!["std", "path", "PathBuf"],
+            path!["std", "collections", "HashMap"],
+        ]);
+        ctx.enter_block();
+        ctx.enter_block();
+        let paths: Vec<&UsePath> = ctx.iter().collect();
+        assert_eq!(paths.len(), 4);
+        assert_eq!(paths[0], &path!["std", "fs", "File"]);
+        assert_eq!(paths[1], &path!["std", "path", "Path"]);
+        assert_eq!(paths[2], &path!["std", "path", "PathBuf"]);
+        assert_eq!(paths[3], &path!["std", "collections", "HashMap"]);
     }
 }
